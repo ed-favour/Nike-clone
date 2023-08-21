b8258850fbffe1fd1b58978eb73cb83c4798f714
ed-favour <izebhijefavouredith@gmail.com>
2023-08-21 16:41:41 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Nav 
+    </section>
+    <section className="padding">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      Superquality
+    </section>
+    <section className="padding py-10">
+      services
+    </section>
+    <section className="padding">
+      specialoffer
+    </section>
+    <section className="padding bg-pale-blue">
+     customerreview
+    </section>
+    <section className="padding-x sm:py-3 py-16 w-full">
+      subscribe
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+      footer
+    </section>
+    
+  </main>
+);
+
+export default App;
